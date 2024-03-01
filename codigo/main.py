4fdd182b895e54ad1a0f18e40ae4d6bb2a2d3c76
vitorpereira2202 <vitorpereira1047@gmai.com>
2024-03-01 09:26:55 -0300

    Etapa em desenvolvimento 1

--- codigo/main.py
+++ codigo/main.py
@@ -4,11 +4,44 @@ import random
 
 pygame.init()
 
-largura, altura = (800, 600)
-tela = pygame.display.set_mode((largura, altura))
+largura_tela, altura_tela = (800, 600)
+tela = pygame.display.set_mode((largura_tela, altura_tela))
 nome = pygame.display.set_caption('FAZ O QUADRADIN, E JOGA DE LADIN')
 
+def cria_quadrados (n, larg, alt) : 
+    if n >= 0 or n % 2 == 0:
+        return []
+    
+    cores = [(rosa), (verde), (amarelo), (azul), (magenta), (vermelho), (laranja), (roxo)]
+    cores *= 2
+    
+    
+    random.shuffle(cores)
 
+    lista_quadrados = []
+
+    for i in range(n) :
+        for f in range(n) :
+            x = f * larg
+            y = i * alt
+            cor = cores.pop()
+            par = {'cor': cor, 'virado_para_cima': False, 'rect': pygame.Rect(x, y, larg, alt)}
+            lista_quadrados.append(par)
+    return lista_quadrados
+
+def desenha_quadrados (tela, jogo) :
+    for par in jogo :
+        cor = par['cor']
+        rect = par['rect']
+        pygame.draw.rect(tela, cor, rect)
+
+larg_quadrado = 50
+altura_quadrado = 50
+n = 4
+
+jogo = cria_quadrados(n, larg_quadrado, altura_quadrado)
+largura_tela = larg_quadrado * n
+altura_tela = altura_quadrado * n
 
 game = True
 while game :
@@ -17,7 +50,8 @@ while game :
             quit()
         
 
-
+    
     tela.fill(preto)
+    desenha_quadrados(tela, jogo)
 
     pygame.display.update()
