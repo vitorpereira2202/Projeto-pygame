637021a8876eb005f3eabcc12ad03d1177d98371
vitorpereira2202 <vitorpereira1047@gmai.com>
2024-03-01 08:35:38 -0300

    codigo fonte para pygame

--- codigo/main.py
+++ codigo/main.py
@@ -1,1 +1,23 @@
 from cores import *
+import pygame
+import random
+
+pygame.init()
+
+largura, altura = (800, 600)
+tela = pygame.display.set_mode((largura, altura))
+nome = pygame.display.set_caption('FAZ O QUADRADIN, E JOGA DE LADIN')
+
+
+
+game = True
+while game :
+    for event in pygame.event.get() :
+        if event.type == pygame.QUIT :
+            quit()
+        
+
+
+    tela.fill(preto)
+
+    pygame.display.update()
