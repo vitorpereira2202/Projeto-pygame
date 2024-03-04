665323363efd9d432a6407007ef24828ca7f4aa0
Caio Ribas <cacazinhoribas7@gmail.com>
2024-03-04 14:31:58 -0300

    Etapa 3

--- codigo/main.py
+++ codigo/main.py
@@ -19,13 +19,24 @@ def cria_quadrados (n, larg, alt, espaco) :
     random.shuffle(cores)
 
     lista_quadrados = []
+    cores_use = {}
+    cor_sorteada = None
+   
 
     for i in range(n) :
         for f in range(n) :
             x = (largura_tela - larg * n - espaco * (n - 1)) // 2 + f * (larg + espaco)
             y = (altura_tela - alt * n - espaco * (n - 1)) // 2.1 + i * (alt + espaco)
-            cor = cores.pop()
-            par = {'cor': cor, 'virado_para_cima': False, 'rect': pygame.Rect(x, y, larg, alt)}
+            cor_sorteada = None
+            while True :
+                cor_sorteada = random.choice(cores)
+                if cor_sorteada in cores_use and cores_use[cor_sorteada] < 2:
+                    cores_use[cor_sorteada] += 1
+                    break
+                elif cor_sorteada not in cores_use :
+                    cores_use[cor_sorteada] = 1
+                    break
+            par = {'cor': branco, 'cor_sorteada': cor_sorteada, 'rect': pygame.Rect(x, y, larg, alt)}
             lista_quadrados.append(par)
     return lista_quadrados
 
@@ -34,6 +45,15 @@ def desenha_quadrados (tela, jogo) :
         cor = par['cor']
         rect = par['rect']
         pygame.draw.rect(tela, cor, rect)
+
+def verifica_clique (posicao, jogo) :
+    for par in jogo :
+        rect = par['rect']
+        if rect.collidepoint(posicao) :
+            if par['cor'] == branco :
+                par['cor'] = par['cor_sorteada']
+            else :
+                par['cor'] = branco
 
 larg_quadrado = 60
 altura_quadrado = 60
@@ -50,6 +70,10 @@ while game :
     for event in pygame.event.get() :
         if event.type == pygame.QUIT :
             quit()
+        elif event.type == pygame.MOUSEBUTTONDOWN :
+            if event.button == 1:
+                pos_mouse = pygame.mouse.get_pos()
+                verifica_clique(pos_mouse, jogo)
         
 
     
